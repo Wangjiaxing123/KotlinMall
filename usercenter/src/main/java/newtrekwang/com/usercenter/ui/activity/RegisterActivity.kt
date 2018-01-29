ceb21c0cfd07f8f7707fd88cf4f754f3b26040b9
wang <408030208@qq.com>
2018-01-29 22:40:42 +0800

    配置了kotlin extentions,anko common

--- usercenter/src/main/java/newtrekwang/com/usercenter/ui/activity/RegisterActivity.kt
+++ usercenter/src/main/java/newtrekwang/com/usercenter/ui/activity/RegisterActivity.kt
@@ -2,12 +2,18 @@ package newtrekwang.com.usercenter.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
 import newtrekwang.com.usercenter.R
+
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mRegistBtn.setOnClickListener {
+            Toast.makeText(applicationContext,"regist",Toast.LENGTH_SHORT).show()
+        }
     }
 }
