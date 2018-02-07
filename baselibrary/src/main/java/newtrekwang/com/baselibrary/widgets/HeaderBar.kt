62d23c044942e79cd16594918acf51c2893d5e17
newtrekWang <408030208@qq.com>
2018-02-07 21:55:00 +0800

    封装HeaderBar

---
+++ baselibrary/src/main/java/newtrekwang/com/baselibrary/widgets/HeaderBar.kt
@@ -0,0 +1,42 @@
+package newtrekwang.com.baselibrary.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import newtrekwang.com.baselibrary.R
+
+import kotlinx.android.synthetic.main.layout_header_bar.view.*
+/**
+ * Created by Newterk on 2018/2/7.
+ */
+class HeaderBar @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var isShowHeader = true
+    private var titleText: String? = null
+    private var rightText: String? = null
+
+    init {
+        var attributeSet = context.obtainStyledAttributes(attrs,R.styleable.HeaderBar)
+        isShowHeader = attributeSet.getBoolean(R.styleable.HeaderBar_isShowBack,true)
+        titleText = attributeSet.getString(R.styleable.HeaderBar_centerTitleText)
+        rightText = attributeSet.getString(R.styleable.HeaderBar_rightText)
+        initView()
+        attributeSet.recycle()
+    }
+
+    private fun initView() {
+        View.inflate(context,R.layout.layout_header_bar,this)
+        mLeftIv.visibility = if (isShowHeader) View.VISIBLE else View.GONE
+        titleText?.let {
+            mTitleTv.text = it
+        }
+
+        rightText?.let {
+            mRightTv.text = it
+            mRightTv.visibility = View.VISIBLE
+        }
+    }
+}
